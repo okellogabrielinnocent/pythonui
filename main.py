7ad74cb8a6a3f36dc2bebb6d38e085b5959ab0ed
okellogabrielinnocent <okellogabrielinnocent@gmail.com>
2019-07-08 14:43:10 +0300

    ft(grid layout):implement grid layout -implement using columnspan parameter in grid() -implement using Entry() -implement using Label() -implement using grid() row and column parameters

--- main.py
+++ main.py
@@ -10,5 +10,24 @@ root = Tk()
 # theLabel.pack()
 #mainloop keeps the window open till it's closed
 
+#grid allows putting layout in rows and coluns
+
+label_1 = Label(root,text="Name")
+label_2 = Label(root,text="Password")
+#entry is used for inputting
+entry_1= Entry(root)
+entry_2= Entry(root)
+
+#by defualt column is always 0
+#stick does alignment using direction N,S,E,W directions
+label_1.grid(row=0,sticky=E)
+label_2.grid(row=1,sticky=E)
+
+entry_1.grid(row=0,column=1)
+entry_2.grid(row=1,column=1)
+
+check =Checkbutton(root,text="Keep me signed in")
+check.grid(columnspan=2)
+
 root.mainloop()
 
