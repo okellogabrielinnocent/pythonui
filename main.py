6a6723755210d4b39be1ad6aad0098287dbe13d7
okellogabrielinnocent <okellogabrielinnocent@gmail.com>
2019-07-08 15:32:30 +0300

    feat(function_binding):implement function binding to layout -write function and bind it using bind function

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 from tkinter import *
 
-#creats blank window
+# creats blank window
 root = Tk()
 
-#label
+# label
 # theLabel = Label(root,text="Home of the God's on earth")
-#position
-#pack puts it to where it finds
+# position
+# pack puts it to where it finds
 # theLabel.pack()
-#mainloop keeps the window open till it's closed
+# mainloop keeps the window open till it's closed
+
+
+def print_name():
+    print("Gabriel Innocent Rockerfeller")
+
+
+button_1 = Button(root, text="Name", command=print_name)
+button_1.pack()
+
+# using bind() to bind and event to a function
+
+
+def print_user_name(event):
+    print("Gabriel Innocent")
+
+
+button_2 = Button(root, text="Name Binder")
+button_2.bind("<Button-1>", print_user_name)
+button_2.pack()
 
 root.mainloop()
-
