fc98a73cc939f4be0df4613c3f2ae7976b057f2a
okellogabrielinnocent <okellogabrielinnocent@gmail.com>
2019-07-08 13:53:08 +0300

    ft(main):implement mainloop - write main function to loop the window

---
+++ main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+#creats blank window
+root = Tk()
+
+#label
+# theLabel = Label(root,text="Home of the God's on earth")
+#position
+#pack puts it to where it finds
+# theLabel.pack()
+#mainloop keeps the window open till it's closed
+
+root.mainloop()
+
