1417c5a27610b3609f490b00156d0f8019b38b66
okellogabrielinnocent <okellogabrielinnocent@gmail.com>
2019-07-08 16:46:18 +0300

    ft(mouse click events):implement binding multiple events from functions - bind multiple events to the functions -use <Button 1>,<Button 2> and <Button 3>

--- main.py
+++ main.py
@@ -10,5 +10,20 @@ root = Tk()
 # theLabel.pack()
 #mainloop keeps the window open till it's closed
 
+# left click and right click
+def left_click(event):
+	print("Left click")
+def right_click(event):
+	print("Right click")
+def middle_click(event):
+	print("Middle Click")
+
+#invisible frame
+frame = Frame(root,width =300, height=250)
+frame.bind("<Button-1>", left_click)
+frame.bind("<Button-2>", middle_click)
+frame.bind("<Button-3>", right_click)
+frame.pack()
+
 root.mainloop()
 
