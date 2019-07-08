8f9c6d56d412811166d209c5cca2b81de0caaa34
okellogabrielinnocent <okellogabrielinnocent@gmail.com>
2019-07-08 14:22:22 +0300

    ft(widgets):implement use of widgets -implemented way of using labels - use of fill parametre in pack() either fill=X or Y - use of side parametre in pack() either LEFT,RIGHT,BOTTOM

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from tkinter import *
 
-#creats blank window
+# creats blank window
 root = Tk()
 
-#label
+# label
 # theLabel = Label(root,text="Home of the God's on earth")
-#position
-#pack puts it to where it finds
+# position
+# pack puts it to where it finds
 # theLabel.pack()
-#mainloop keeps the window open till it's closed
+# mainloop keeps the window open till it's closed
 
+# bg is background color while fg is front color
+one = Label(root, text="One", bg="red", fg="white")
+one.pack()
+two = Label(root, text="Two", bg="green", fg="black")
+# grows the two widget to parent size on x direction
+two.pack(fill=X)
+
+three = Label(root, text="Three", bg="blue", fg="white")
+three.pack(side=LEFT, fill=Y)
 root.mainloop()
-
