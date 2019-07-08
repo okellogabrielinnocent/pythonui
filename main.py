b27b934727ba5d33a3150b8e12c28caea96c2f8c
okellogabrielinnocent <okellogabrielinnocent@gmail.com>
2019-07-08 19:42:58 +0300

    ft(classes):implement class base tkinker -implement use of class in tkinker

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 from tkinter import *
 
-#creats blank window
-root = Tk()
+# creats blank window
+# root = Tk()
 
-#label
+# label
 # theLabel = Label(root,text="Home of the God's on earth")
-#position
-#pack puts it to where it finds
+# position
+# pack puts it to where it finds
 # theLabel.pack()
-#mainloop keeps the window open till it's closed
+# mainloop keeps the window open till it's closed
+
+
+class GabsButton:
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+
+        self.print_button = Button(
+            frame, text="Message", command=self.print_message)
+        self.print_button.pack(side=LEFT)
+
+        self.quit_button = Button(frame, text="Quit", command=frame.quit)
+        self.quit_button.pack(side=LEFT)
+
+    def print_message(self):
+        print("We rock. Yeah!!!")
+
+
+root = Tk()
+g = GabsButton(root)
 
 root.mainloop()
-
